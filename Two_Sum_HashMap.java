3d7bbec5840826c88968b5c7535e133783c89677
Sowmith Pentaparthy <sp070771@cerner.net>
2020-03-30 08:41:15 -0500

    Two Sum HashMap

---
+++ Two_Sum_HashMap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        Map<Integer, Integer> solution = new HashMap<>();
+       for(int i = 0; i <= length-1; i++){
+           int difference = target - nums[i];
+           if(solution.containsKey(difference)){
+               return new int[]{solution.get(difference), i};
+           }
+           solution.put(nums[i],i);
+        }
+        return null;
+    }
+}
